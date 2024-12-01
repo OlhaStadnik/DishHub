df229e953d3b573f0210b99051ab1bc5d388cba4
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-01 19:19:14 +0200

    " created models by app recipe_manager"

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "accounts.CookUser"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
--- recipe_manager/models.py
+++ recipe_manager/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+class DishType(models.Model):
+    name = models.CharField(max_length=70, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name='dishes')
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='dishes')
+
+    def __str__(self):
+        return (f"{self.dish_type}: "
+                f"{self.name}( {self.price}) {self.description} - {self.cooks}") #self.name

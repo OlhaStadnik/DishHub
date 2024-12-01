c3c0dc2a8ea9c449926909e6f7f483d6189163e3
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-01 18:46:00 +0200

    "created user models and admin panels"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CookUser
 
-# Register your models here.
+@admin.register(CookUser)
+class CookUserAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    search_fields = UserAdmin.search_fields
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("years_of_experience",)}),
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CookUser(AbstractUser):
+    years_of_experience = models.IntegerField(default=0, blank=True, null=True)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=70, unique=True)
+
+    class Meta:
+        ordering = ['username']
+
+
+    def __str__(self):
+        return self.username
+

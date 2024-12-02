22f8ecb5d356509ef4a7e1201ee6b4dfa0c7746b
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-02 16:04:58 +0200

    "make urls for recipe_manager"

---
+++ recipe_manager/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from .views import (
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView,
+    DishListView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+)
+
+urlpatterns = [
+    path("dish_type/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish_type/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish_type/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish_type/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
+]
+
+app_name = "recipe_manager"

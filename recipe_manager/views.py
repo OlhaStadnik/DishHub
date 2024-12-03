0ccb66aff2c37fa5ee04e1091251de43d3573e20
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-03 17:11:49 +0200

    "create forms for recipe_manager app and update views for them"

--- recipe_manager/views.py
+++ recipe_manager/views.py
@@ -2,16 +2,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic import View
 
 from recipe_manager.models import DishType, Dish
 
+class HomeView(generic.TemplateView):
+    template_name = "home.html"
 
-class DishTypeListView(LoginRequiredMixin, generic.ListView):
+
+class DishTypeListView(generic.ListView):
     model = DishType
     template_name = "recipe_manager/dish_type_list.html"
     context_object_name = "dish_types"
     queryset = DishType.objects.all()
-    paginate_by = 5
+    paginate_by = 6
 
 class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
     model = DishType
@@ -31,7 +35,7 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
     success_url = reverse_lazy("dishes:dish-type-list")
 
 # Dish Views
-class DishListView(LoginRequiredMixin, generic.ListView):
+class DishListView(generic.ListView):
     model = Dish
     template_name = "recipe_manager/dish_list.html"
     context_object_name = "dishes"
